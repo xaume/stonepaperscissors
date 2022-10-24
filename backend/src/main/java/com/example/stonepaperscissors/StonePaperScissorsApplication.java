03e1b64b3ee34cac4c39bf30e9952c29c2607528
Jaime Jimenez <en.xaume@gmail.com>
2022-10-24 02:22:33 +0200

    Initial commit with backend.

---
+++ backend/src/main/java/com/example/stonepaperscissors/StonePaperScissorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.stonepaperscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonePaperScissorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StonePaperScissorsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/stonepaperscissors/StonePaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stonepaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonePaperScissorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
